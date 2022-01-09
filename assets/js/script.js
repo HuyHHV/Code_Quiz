e28238d8e3510b298e6fd250384e46b210cb76f8
Huy Vo <vohuy819@gmail.com>
2022-01-09 20:35:19 +1030

    demo-without local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,105 @@
-var body = document.body;
+var secondsLeft = 101; 
+var index = 0;
 var timeEl = document.querySelector(".time");
-var quizList = document.createElement("ul");
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
-var secondsLeft = 60;
+var startBtn = document.querySelector("#startBtn");
+var questionTitle = document.querySelector("#questionTitle");
+var box= document.querySelector(".box");
+var listQ = document.querySelector("#listQ")
+var notice= document.querySelector("#alert");
+var score
+var nextQ
+var check
+var questions = [
+  {
+      title: "Commonly used data type Do Not include:---",
+      choices: ["strings","booleance","alerts", "numbers"],
+      answer : "alerts"    
+  },
+  {
+      title: "The condition in an if/else statement is enclosed within:---",
+      choices: ["quotes","Curly brackets","parentheses", "square brackets"],
+      answer : "parentheses"    
+  },
+  {
+      title: "Arrays in JavaScript can be used to store:---",
+      choices: ["numbers and strings","others Arrays","booleances", "all of the above"],
+      answer : "all of the above"    
+  },
+  {
+      title: "String values must be enclosed within --- when being assigned to variables ",
+      choices: ["commas","curly brackets","quotes","parentheses"],
+      answer : "quotes"    
+  },
+  {
+      title: "A very useful tool used during development and debugging for printing content to the debugger is:---",
+      choices: ["JavaScript","terminal/bash","alerts", "console.log"],
+      answer : "console.log"    
+  },
+]
 
-var q1 = {
-    question: "ab_dfg",
-    answer: ["c","a","d"],
-} 
+// Press "start" to start the quiz
 
-function setTime() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = "time: " + secondsLeft + "s";
+startBtn.addEventListener("click", start);
+
+function start() {
+  console.log(questions.length);
+  timer();
+  startBtn.style.display = "none" ;
+  nextQ = questions[index];
+  displayQ(nextQ);
+}
+// Start the timer
+function timer() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = "time: " + secondsLeft + "s";
+
+    if(secondsLeft <= 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+// Display question 
+function displayQ(question) {
+  console.log(index);
+  questionTitle.textContent = question.title
+  for (var i =0; i < question.choices.length; i++){
+    var button = document.createElement("button");
+    button.textContent = question.choices[i];
+    button.addEventListener("click",displayNextQ)
+    button.classList.add("btn")
+    listQ.appendChild(button);
+  };  
+}
+// Choose correct anwser by pressing one of them -> right/wrong + penalty + 
+
+function markingQ () {
+
+  if (check) {
+    notice.textContent = "Correct!!"
+  }
+  else {
+    notice.textContent = "Incorrect!!"
+    secondsLeft -= 10;
+  }
+ 
+}
+// move to next question
+function displayNextQ(e) {
+  markingQ
+  check = (e.target.innerText == nextQ.answer);
+  index++;
   
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Calls function to create and append image
-       
-      }
-  
-    }, 1000);
+  if (index < questions.length) {
+    nextQ = questions[index];
+    questionTitle.innerHTML = "";
+    listQ.innerHTML = "";
+    displayQ(nextQ);
+  }
+  else if (index >= questions.length) {
+    score = secondsLeft
+  }
 }
 
-function quiz() {
-
-li1.textContent = q1.question;
-li2.textContent = q1.answer[0];
-li3.textContent = q1.answer[1];
-li4.textContent = q1.answer[2];
-
-body.appendChild(quizList);
-quizList.appendChild(li1);
-quizList.appendChild(li2);
-quizList.appendChild(li3);
-quizList.appendChild(li4);
-}
-
-function answer() {
-  li2.addEventListener("click",function(){
-    secondsLeft -= 10;
-  })
-  li2.addEventListener("click",function(){
-    secondsLeft -= 10;
-  })
-  li2.addEventListener("click",function(){
-    secondsLeft -= 10;
-  })
-}
-
-setTime()
-quiz();
-answer()
